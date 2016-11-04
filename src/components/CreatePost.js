3c401c8bcc56532a320e47a35ffe0ebf87d4fc5c
Niles McGiver <niles@finedesigngroup.com>
2016-11-04 09:17:27 -0700

    added in posts form

---
+++ src/components/CreatePost.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default class CreatePost extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { postsArr: this.props.postdata.posts};
+    this.createPost = this.createPost.bind(this);
+  }
+
+  createPost(e) {
+    e.preventDefault();
+    this.props.addPosts(this.state.postsArr);
+  }
+
+  render(){
+    return (
+      <button
+        className='newPost'
+        onClick={this.createPost}>
+        Make A New Post
+      </button>
+    );
+  }
+}
---
+++ src/components/PostForm.js
@@ -0,0 +1,58 @@
+import React from "react"
+import Link from "./Links";
+import Input from "./Inputs"
+
+export default class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { post: this.props.postdata };
+  }
+
+  updatePost(data, field) {
+    // re render this posts
+    let updatedPost = this.state.post;
+    switch (field) {
+      case 'subject':
+          updatedPost.subject = data;
+          break;
+      case 'text':
+          updatedPost.subject = data;
+          break;
+      }
+    debugger
+    this.setState({ post: updatedPost });
+  }
+
+  updateLink(link) {
+    // re render this posts
+    this.setState({ posts: link });
+  }
+
+
+  render(){
+    var postObj = this.props.postdata;
+    let linksObj = postObj.links;
+    const linksMap = linksObj.map((link, idx) => {
+      return (
+        <div key={idx}>
+          <Input inputdata={link.link_text} updatePostObj={this.updateLink} />
+          <Input inputdata={link.link} updatePostObj={this.updateLink} />
+        </div>
+      );
+    })
+    return (
+      <div className="App-section">
+        <h3>Edit Post</h3>
+        <form>
+          <Input inputdata={this.props.postdata.subject} inputfield='subject' updatepost={this.updatePost.bind(this)}/>
+          <br></br>
+          <Input inputdata={this.props.postdata.text} inputfield='text' updatepost={this.updatePost.bind(this)}/>
+          <br></br>
+          <div>
+            { linksMap }
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
