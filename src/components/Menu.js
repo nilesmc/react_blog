c8c3c8b826eff5dcad597c507293ca9bedcd99da
Niles McGiver <niles@finedesigngroup.com>
2016-12-01 19:01:48 -0800

    got hamburger nav up and running

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,15 @@
 import React from "react"
+import RBN from 'react-burger-nav'
 
 class Menu extends React.Component{
-  showSettings(e) {
-    e.preventDefault();
-    console.log('you get a cookie');
-  }
+
   render(){
     return (
-      <div>
+      <RBN>
         <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="login" className="menu-item" href="/login">Login</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </div>
+      </RBN>
     );
   }
 };
